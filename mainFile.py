8ca0460c215166205336ae0744498447763b3dd7
MoAdelAbdelrahman <58152328+MoAdelAbdelrahman@users.noreply.github.com>
2021-09-23 18:11:09 +0200

    finishing the base implementation

--- mainFile.py
+++ mainFile.py
@@ -121,8 +121,31 @@ class mountain:
             return True
         return False
 
+class Base:
+    vel = 5
+    width = base_img.get_width()
+    img = base_img
 
-def draw(win, plane):
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.width
+
+    def move(self):
+        self.x1 -= self.vel
+        self.x2 -= self.vel
+
+        if self.x1 + self.width < 0:
+            self.x1 = self.x2 + self.width
+        if self.x2 + self.width < 0:
+            self.x2 = self.x1 + self.width
+
+    def draw(self, win):
+        win.blit(self.img, (self.x1, self.y))
+        win.blit(self.img, (self.x2, self.y))
+
+
+def draw_win(win, plane):
     win.blit(bg_img, (0, 0))
     plane.draw(win)
     pygame.display.update()
@@ -139,7 +162,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
         plane.move()
-        draw(win, plane)
+        draw_win(win, plane)
     pygame.quit()
     quit()
 
